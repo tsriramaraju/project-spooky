685a7c6dd7f2622497653d49173f12af3efcb6b2
Sriram <tsriramaraju@gmail.com>
2022-08-03 00:17:33 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,8 @@ import dns from 'dns';
 dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
+  base: 'project-spooky',
   server: {
     host: '0.0.0.0',
   },
-  base: '/project-spooky/',
 });
