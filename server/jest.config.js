4830361b752d9380f26bf5c31ede13083d99fe8a
Sriram <tsriramaraju@gmail.com>
2022-08-02 16:56:44 +0530

    dummy env in jest test file

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,3 +1,7 @@
+process.env.PORT = "4545";
+process.env.MONGO_URL = "Some random url";
+process.env.SLACK_SECRET = "Some random secret";
+
 module.exports = {
   verbose: true,
   preset: "ts-jest",
