3fb730a96c6fd4250f8d0d0745b1bc513bba4b81
Sriram <tsriramaraju@gmail.com>
2022-08-03 16:33:14 +0530

    added little flare for real time heart update

--- client/src/components/react/votesCount/index.tsx
+++ client/src/components/react/votesCount/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { channel } from '../../../main';
+import HeartIcon from '../hearIcon';
+import { Player } from '@lottiefiles/react-lottie-player';
 import styles from './styles.module.scss';
+import animationData from '../../../assets/lottie/heart2.json';
 
 interface props {
   count: number;
@@ -9,12 +12,33 @@ interface props {
 
 const VotesCount = ({ count, id }: props) => {
   const [voteCount, setVoteCount] = React.useState(count);
+  const [playAnimation, setPlayAnimation] = React.useState(false);
 
   channel.bind(id, (data: any) => {
     setVoteCount(data.message);
+    setPlayAnimation(true);
+    setTimeout(() => {
+      setPlayAnimation(false);
+    }, 2500);
   });
 
-  return <div className={styles.container}>{voteCount}</div>;
+  if (voteCount === 0) return <div></div>;
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.count}>{voteCount}</p>
+      <HeartIcon className={styles.icon} />
+      {playAnimation && (
+        <div className={styles.lottie}>
+          <Player
+            autoplay
+            src={animationData}
+            style={{ height: '100%', width: '100%' }}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default VotesCount;
